5bcb810a7f071569d5915ad2ac7bcc7c3fe0ef99
github kakerutokunaga <icecream.tokeru@gmail.com>
2021-01-11 19:00:35 +0900

    change metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `kakeru-blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Kakeru Tokunaga`,
+      summary: `Belogns to React inc, PM team`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    siteUrl: `https://kakeru-blog.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `kuu_cyn`,
     },
   },
   plugins: [
